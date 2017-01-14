528b3292472d471a89f62a1815b7addb8dc9a5fc
Kawamura <3bjk1213@xeon2.localdomain>
2017-01-14 18:57:50 +0900

    add file

---
+++ python/train_nn_online.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+import sys, os
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+# データの読み込み
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+iters_num = 30000  # 繰り返しの回数を適宜設定する
+train_size = x_train.shape[0]
+batch_size = 1
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+for i in range(iters_num):
+    mask = np.random.choice(train_size, 1)
+    x_data = x_train[mask]
+    t_data = t_train[mask]
+    
+    # 勾配の計算
+    grad = network.gradient(x_data, t_data) # 誤差逆伝搬法
+    
+    # パラメータの更新
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    if i % 1000 == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+# グラフの描画
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
