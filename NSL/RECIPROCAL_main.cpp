a850f95424167d836da700cd0284c52ad846ac4b
Mariya <3bjk1213@gmail.com>
2017-01-31 19:12:22 +0900

    [modify] シミュレーション完了

--- NSL/RECIPROCAL_main.cpp
+++ NSL/RECIPROCAL_main.cpp
@@ -12,6 +12,7 @@ RECIPROCAL *rec_i;
 
 sc_clock clk("clk");
 sc_signal<bool> reset("reset");
+sc_signal<sc_uint<2> > count;
 sc_signal<sc_uint<19> > x;
 sc_signal<sc_uint<19> > y;
 sc_signal<sc_uint<1> > calc;
@@ -46,60 +47,48 @@ int dec2int(double x){
 
 int sc_main(int argc, char **argv){
 	double x_dec, answer;
-//	FILE *fp;
+	FILE *fp;
 
 	rec_i = new RECIPROCAL("rec_i");
 	rec_i->m_clock(clk);
 	rec_i->p_reset(reset);
+	rec_i->count(count);
 	rec_i->x(x);
 	rec_i->y(y);
 	rec_i->calc(calc);
 
 	sc_start(0, SC_NS);
-	reset = 1;
-	sc_start(1, SC_NS);
 	reset = 0;
+	sc_start(1, SC_NS);
+	reset = 1;
 
-/*
 	fp = popen("gnuplot -persist", "w");
 	fprintf(fp, "set terminal png\n");
-	fprintf(fp, "set output 'sigmoid_sim_01.png'\n");
+	fprintf(fp, "set output 'reciprocal_sim_01.png'\n");
 	fprintf(fp, "set multiplot\n");
 	fprintf(fp, "set xrange [-7:7]\n");
 	fprintf(fp, "set yrange [0:1]\n");
 	fprintf(fp, "set xlabel 'x'\n");
 	fprintf(fp, "set ylabel 'y'\n");
 	fprintf(fp, "unset key\n");
-*/
 
 	srand(time(NULL));
-	x_dec = (double)rand()/RAND_MAX*9.0+1.0;
-	x = dec2int(x_dec);
-	calc = 1;
-	sc_start(1, SC_NS);
-	cout << "x = " << x_dec << "     " << "y = " << int2dec(y.read()) << "\n";
-	cout << int2dec(rec_i->a.read()) << "\n";
-	sc_start(1, SC_NS);
-	cout << "x = " << x_dec << "     " << "y = " << int2dec(y.read()) << "\n";
-	cout << int2dec(rec_i->a.read()) << "\n";
-	sc_start(1, SC_NS);
-	cout << "x = " << x_dec << "     " << "y = " << int2dec(y.read()) << "\n";
-	cout << int2dec(rec_i->a.read()) << "\n";
-	sc_start(1, SC_NS);
-	cout << "x = " << x_dec << "     " << "y = " << int2dec(y.read()) << "\n";
-	cout << int2dec(rec_i->a.read()) << "\n";
-
-/*
 	fprintf(fp, "plot '-' with points\n");
 	for(int i=0 ; i<100 ; i++){
-		x_dec = (double)rand()/RAND_MAX*12.0-6.0;
-		x = (x_dec>=0 ? 0 : 262144) + dec2int(fabs(x_dec));
+		x_dec = (double)rand()/RAND_MAX*9.0+1.0;
+		x = dec2int(x_dec);
+		count = 2;
 		calc = 1;
-		sc_start(1, SC_NS);
+		sc_start(1, SC_NS);	// func呼び出し
+		calc = 0;
+		sc_start(1, SC_NS); // init
+		sc_start(1, SC_NS); // exec
+		sc_start(1, SC_NS); // exec
 		fprintf(fp, "%lf %lf\n", x_dec, int2dec(y.read()));
 	}
 	fprintf(fp, "e\n");
 
+/*
 	fprintf(fp, "plot '-' with points\n");
 	for(int i=0 ; i<100 ; i++){
 		x_dec = (double)rand()/RAND_MAX*4.0-2.0;
@@ -109,17 +98,16 @@ int sc_main(int argc, char **argv){
 		fprintf(fp, "%lf %lf\n", x_dec, int2dec(y.read()));
 	}
 	fprintf(fp, "e\n");
+*/
 
 	fprintf(fp, "plot '-' with lines\n");
-	for(double i=-7.0 ; i<7.0 ; i+=0.1){
-		answer = sigmoid(i);
-		fprintf(fp, "%lf %lf\n", i, answer);
+	for(double i=1.0 ; i<10.0 ; i+=0.1){
+		fprintf(fp, "%lf %lf\n", i, 1.0/i);
 	}
 	fprintf(fp, "e\n");
 
 	fprintf(fp, "unset multiplot\n");
 	pclose(fp);
-*/
 
 	return 0;
 }
